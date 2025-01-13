11933b42e663c9692c4b94ef107c4b0ffcc85bea
Patrik Pukan <patrik.pukan@gmail.com>
2025-01-13 16:19:09 +0100

    test commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import { useMemo } from "react";
 import {
-  MaterialReactTable,
-  useMaterialReactTable,
-  type MRT_ColumnDef,
-} from "material-react-table";
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableContainer,
+  TableBody,
+} from "@mui/material";
 import tableStyles from "./tableStyles";
-//example data type
+
 type Person = {
   name: {
     firstName: string;
     lastName: string;
   };
+  age: number;
+  employed: boolean;
+  jobTitle: string;
   address: string;
   city: string;
   state: string;
 };
 
-//nested data is ok, see accessorKeys in ColumnDef below
 const data: Person[] = [
   {
     name: {
       firstName: "John",
       lastName: "Doe",
     },
+    age: 35,
+    employed: true,
+    jobTitle: "Software Engineer",
     address: "261 Erdman Ford",
     city: "East Daphne",
     state: "Kentucky",
@@ -32,6 +39,9 @@ const data: Person[] = [
       firstName: "Jane",
       lastName: "Doe",
     },
+    age: 28,
+    employed: true,
+    jobTitle: "Marketing Manager",
     address: "769 Dominic Grove",
     city: "Columbus",
     state: "Ohio",
@@ -41,6 +51,9 @@ const data: Person[] = [
       firstName: "Joe",
       lastName: "Doe",
     },
+    age: 45,
+    employed: false,
+    jobTitle: "none",
     address: "566 Brakus Inlet",
     city: "South Linda",
     state: "West Virginia",
@@ -50,6 +63,9 @@ const data: Person[] = [
       firstName: "Kevin",
       lastName: "Vandy",
     },
+    age: 31,
+    employed: true,
+    jobTitle: "Financial Analyst",
     address: "722 Emie Stream",
     city: "Lincoln",
     state: "Nebraska",
@@ -59,11 +75,15 @@ const data: Person[] = [
       firstName: "Joshua",
       lastName: "Rolluffs",
     },
+    age: 42,
+    employed: false,
+    jobTitle: "none",
     address: "32188 Larkin Turnpike",
     city: "Omaha",
     state: "Nebraska",
   },
 ];
+
 interface HeaderConfig {
   rowNum: number;
   colNum: number;
@@ -73,89 +93,112 @@ interface HeaderConfig {
 
 const headerConfig: HeaderConfig[] = [
   { rowNum: 0, colNum: 0, colSpan: 2, title: "Personal Information" },
-  { rowNum: 0, colNum: 2, colSpan: 3, title: "Location Details" },
+  { rowNum: 0, colNum: 2, colSpan: 3, title: "Employment Information" },
+  { rowNum: 0, colNum: 5, colSpan: 3, title: "Location Details" },
   { rowNum: 1, colNum: 0, colSpan: 1, title: "First Name" },
   { rowNum: 1, colNum: 1, colSpan: 1, title: "Last Name" },
-  { rowNum: 1, colNum: 2, colSpan: 1, title: "Address" },
-  { rowNum: 1, colNum: 3, colSpan: 1, title: "City" },
-  { rowNum: 1, colNum: 4, colSpan: 1, title: "State" },
+  { rowNum: 1, colNum: 2, colSpan: 1, title: "Age" },
+  { rowNum: 1, colNum: 3, colSpan: 1, title: "Employed" },
+  { rowNum: 1, colNum: 4, colSpan: 1, title: "Job Title" },
+  { rowNum: 1, colNum: 5, colSpan: 1, title: "Address" },
+  { rowNum: 1, colNum: 6, colSpan: 1, title: "City" },
+  { rowNum: 1, colNum: 7, colSpan: 1, title: "State" },
 ];
+
 const App = () => {
-  const getCellStyle = (rowNum: number, colNum: number) => {
-    const cellStyle = tableStyles.find(
-      (style) => style.rowNum === rowNum && style.colNum === colNum
-    );
-    return cellStyle?.style || {};
+  // Helper function to get headers for a specific row
+  const getHeadersForRow = (rowNumber: number) => {
+    return headerConfig
+      .filter((header) => header.rowNum === rowNumber)
+      .sort((a, b) => a.colNum - b.colNum);
   };
-  //should be memoized or stable
-  const columns = useMemo<MRT_ColumnDef<Person>[]>(
-    () => [
-      {
-        header: headerConfig[0].title,
-        columns: [
-          {
-            accessorKey: "name.firstName",
-            header: headerConfig[2].title,
-            Cell: ({ row, cell }) => (
-              <div style={getCellStyle(row.index, 0)}>
-                {cell.getValue<string>()}
-              </div>
-            ),
-          },
-          {
-            accessorKey: "name.lastName",
-            header: headerConfig[3].title,
-            Cell: ({ row, cell }) => (
-              <div style={getCellStyle(row.index, 1)}>
-                {cell.getValue<string>()}
-              </div>
-            ),
-          },
-        ],
-      },
-      {
-        header: headerConfig[1].title,
-        columns: [
-          {
-            accessorKey: "address",
-            header: headerConfig[4].title,
-            Cell: ({ row, cell }) => (
-              <div style={getCellStyle(row.index, 2)}>
-                {cell.getValue<string>()}
-              </div>
-            ),
-          },
-          {
-            accessorKey: "city",
-            header: headerConfig[5].title,
-            Cell: ({ row, cell }) => (
-              <div style={getCellStyle(row.index, 3)}>
-                {cell.getValue<string>()}
-              </div>
-            ),
-          },
-          {
-            accessorKey: "state",
-            header: headerConfig[6].title,
-            Cell: ({ row, cell }) => (
-              <div style={getCellStyle(row.index, 4)}>
-                {cell.getValue<string>()}
-              </div>
-            ),
-          },
-        ],
-      },
-    ],
-    []
-  );
-  const table = useMaterialReactTable({
-    columns,
-    data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
-  });
+
+  // Get the maximum row number to determine number of header rows needed
+  const maxRowNum = Math.max(...headerConfig.map((header) => header.rowNum));
 
   return (
     <div>
-      <MaterialReactTable table={table} />
+      <TableContainer>
+        <Table
+          sx={{
+            border: "1px solid rgba(224, 224, 224, 1)",
+            borderCollapse: "collapse",
+          }}
+        >
+          <TableHead>
+            {/* Render header rows */}
+            {Array.from({ length: maxRowNum + 1 }, (_, index) => (
+              <TableRow key={`header-row-${index}`}>
+                {getHeadersForRow(index).map((header, headerIndex) => (
+                  <TableCell
+                    key={`header-cell-${index}-${headerIndex}`}
+                    align="center"
+                    colSpan={header.colSpan}
+                    sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                  >
+                    {header.title}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableHead>
+          <TableBody>
+            {/* Render data rows */}
+            {data.map((row, rowIndex) => (
+              <TableRow key={`data-row-${rowIndex}`}>
+                <TableCell
+                  align="center"
+                  sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                >
+                  {row.name.firstName}
+                </TableCell>
+                <TableCell
+                  sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                  align="center"
+                >
+                  {row.name.lastName}
+                </TableCell>
+                <TableCell
+                  sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                  align="center"
+                >
+                  {row.age}
+                </TableCell>
+                <TableCell
+                  sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                  align="center"
+                >
+                  {row.employed ? "Yes" : "No"}
+                </TableCell>
+                <TableCell
+                  sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                  align="center"
+                >
+                  {row.jobTitle}
+                </TableCell>
+                <TableCell
+                  sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                  align="center"
+                >
+                  {row.address}
+                </TableCell>
+                <TableCell
+                  sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                  align="center"
+                >
+                  {row.city}
+                </TableCell>
+                <TableCell
+                  sx={{ border: "1px solid rgba(224, 224, 224, 1)" }}
+                  align="center"
+                >
+                  {row.state}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
